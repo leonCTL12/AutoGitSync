106d5ecc4aaa55f659e3eec0a9bf14b218557f6f
Leon Chan <leon.chan@thisisgusto.com>
2024-02-23 11:28:14 +0800

    securely store access token (WIP)

--- code_auto_back_uper/src/data_structures/config.rs
+++ code_auto_back_uper/src/data_structures/config.rs
@@ -4,16 +4,22 @@ use std::collections::HashSet;
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub watching_folders: HashSet<String>,
+    pub encrypted_access_token: String,
 }
 
 impl Config {
     pub fn new() -> Config {
         Config {
             watching_folders: HashSet::new(),
+            encrypted_access_token: String::new(),
         }
     }
 
     pub fn insert_watching_folder(&mut self, folder: String) {
         self.watching_folders.insert(folder);
+    }
+
+    pub fn update_encrypted_access_token(&mut self, token: String) {
+        self.encrypted_access_token = token;
     }
 }
