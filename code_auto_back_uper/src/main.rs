25426adceb465bee2894e4f42a8f9c694f5c20ed
Leon Chan <leon.chan@thisisgusto.com>
2024-03-04 10:45:56 +0800

    added code to test the gitignore crate, it is working

--- code_auto_back_uper/src/main.rs
+++ code_auto_back_uper/src/main.rs
@@ -1,86 +1,60 @@
-mod backup_executor;
-mod config_manager;
-mod cross_platform_constant;
-mod data_structures;
-mod file_change_watcher;
-mod repository_instance;
-mod temp_clone_repo;
-mod utilities;
-use structopt::StructOpt;
-use utilities::file_system;
+//This is to test if the ignore crate work properly
 
-#[derive(StructOpt)]
-struct Cli {
-    #[structopt(subcommand)]
-    cmd: Command,
-}
+use std::path::Path;
 
-#[derive(StructOpt)]
-enum Command {
-    #[structopt(about = "Watches a Folder")]
-    Add {
-        #[structopt(help = "The folder to backup when it is updated")]
-        folder: String,
-    },
-    #[structopt(about = "Watch all the git repo in this folder (not recursive)")]
-    AddWorkspace {
-        #[structopt(help = "The root folder to watch all the git repos in it")]
-        folder: String,
-    },
-    #[structopt(about = "List the watched folders")]
-    List,
-    #[structopt(about = "Remove a watched folder")]
-    Remove {
-        #[structopt(help = "The folder to remove from the watch list")]
-        folder: String,
-    },
-    #[structopt(about = "Start to periodically backup the watched folders")]
-    Run,
-    #[structopt(about = "Store the ssh private key path")]
-    SetSSH {
-        #[structopt(help = "The ssh private key path")]
-        ssh_key_path: String,
-    },
-    #[structopt(about = "Store the Personal Access Token")]
-    SetPAT {
-        #[structopt(help = "The personal access token")]
-        token: String,
-    },
-}
+use ignore::gitignore::Gitignore;
 
 fn main() {
-    let args = Cli::from_args();
+    let git_ignore = create_gitignore("/Users/leonchan/Desktop/test_git_ignore");
+    let git_ignore = match git_ignore {
+        Ok(git_ignore) => git_ignore,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
 
-    match args.cmd {
-        Command::Add { folder } => {
-            config_manager::add_watched_folder(&folder);
-        }
-        Command::AddWorkspace { folder } => {
-            let repos = match file_system::get_sub_folders(&folder) {
-                Ok(repos) => repos,
-                Err(e) => {
-                    panic!("Failed to get sub folders: {}", e);
-                }
-            };
-            for repo in repos {
-                config_manager::add_watched_folder(&repo);
-            }
-        }
-        Command::List => {
-            config_manager::list_watched_folder();
-        }
-        Command::Remove { folder } => {
-            config_manager::remove_watched_folder(&folder);
-        }
-        Command::Run => {
-            println!("Start to periodically backup the watched folders");
-            backup_executor::BackupExecutor::new().start();
-        }
-        Command::SetSSH { ssh_key_path } => {
-            config_manager::set_ssh_key_path(ssh_key_path);
-        }
-        Command::SetPAT { token } => {
-            config_manager::set_pat(token);
-        }
+    test_git_ignore(
+        &git_ignore,
+        &String::from("../test_git_ignore"),
+        &String::from("../test_git_ignore/a.txt"),
+    );
+
+    test_git_ignore(
+        &git_ignore,
+        &String::from("../test_git_ignore"),
+        &String::from("../test_git_ignore/log/b.mp4"),
+    );
+
+    test_git_ignore(
+        &git_ignore,
+        &String::from("../test_git_ignore"),
+        &String::from("../test_git_ignore/c.png"),
+    );
+}
+
+fn create_gitignore(path: &str) -> Result<Gitignore, String> {
+    let gitignore_path = Path::new(path).join(".gitignore");
+    let var_name = Gitignore::new(gitignore_path);
+    let git_ignore = var_name;
+
+    if git_ignore.1.is_some() {
+        return Err(format!("Failed to create gitignore for {}", path));
+    }
+
+    let git_ignore = git_ignore.0;
+    Ok(git_ignore)
+}
+
+pub fn test_git_ignore(git_ignore: &Gitignore, repo_path: &String, file_path: &String) {
+    let absolute_path = Path::new(file_path);
+    let relative_path = absolute_path.strip_prefix(repo_path).unwrap();
+
+    let is_ignored = git_ignore.matched(relative_path, false).is_ignore();
+
+    if is_ignored {
+        println!("{} is ignored", file_path);
+    } else {
+        println!("{} is not ignored", file_path);
     }
 }
