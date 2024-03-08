cc53833dcea78bba9388d6a434792be26dda117b
Leon Chan <leon.chan@thisisgusto.com>
2024-03-08 11:52:35 +0800

    delete the temp clone repo before duplicating

--- src/utilities/copy_dir_api_wrapper.rs
+++ src/utilities/copy_dir_api_wrapper.rs
@@ -1,7 +1,21 @@
 use copy_dir::copy_dir;
 
+use super::file_system::is_path_exist;
+
 pub fn copy_directory(directory_path: &str) -> Result<String, String> {
     let destination = format!("{}_temp_clone", directory_path);
+
+    if is_path_exist(&destination) {
+        match std::fs::remove_dir_all(&destination) {
+            Ok(_) => {}
+            Err(e) => {
+                return Err(format!(
+                    "Failed to remove the existing directory {}: {}",
+                    destination, e,
+                ));
+            }
+        }
+    }
     match copy_dir(directory_path, &destination) {
         Ok(_) => {
             println!("{} has been copied to {}", directory_path, destination);
